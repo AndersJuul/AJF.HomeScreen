fcedabb38fb33caccdb3078ed228569af30c8e48
AndersJuul <andersjuulsfirma@gmail.com>
2018-11-18 11:09:40 +0100

    Update HomeController.cs

--- Src/AJF.HomeScreen.WebFront/Controllers/HomeController.cs
+++ Src/AJF.HomeScreen.WebFront/Controllers/HomeController.cs
@@ -10,30 +10,70 @@ namespace AJF.HomeScreen.WebFront.Controllers
     {
         public ActionResult Index()
         {
-            var schoolScheduleIsa = new SchoolSchedule()
+            var schoolScheduleIsa = new SchoolSchedule
             {
                 SchDays = new[]
                 {
                     new SchDay
                     {
-                        DayOfWeek = DayOfWeek.Thursday,
+                        DayOfWeek = DayOfWeek.Monday,
                         SchItems = new[]
                         {
-                            new SchItem { FromTime = "8.00", ToTime = "8.55", What = "US"},
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
                         }
                     },
                     new SchDay
                     {
-                    DayOfWeek = DayOfWeek.Friday,
-                    SchItems = new[]
+                        DayOfWeek = DayOfWeek.Tuesday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
+                        }
+                    },
+                    new SchDay
                     {
-                        new SchItem { FromTime = "8.00", ToTime = "8.55", What = "US"},
-                    }
+                        DayOfWeek = DayOfWeek.Wednesday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
+                        }
+                    },
+                    new SchDay
+                    {
+                        DayOfWeek = DayOfWeek.Thursday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
+                        }
+                    },
+                    new SchDay
+                    {
+                        DayOfWeek = DayOfWeek.Friday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
+                        }
+                    },
+                    new SchDay
+                    {
+                        DayOfWeek = DayOfWeek.Saturday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
+                        }
+                    },
+                    new SchDay
+                    {
+                        DayOfWeek = DayOfWeek.Sunday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
+                        }
                     }
                 }
             };
 
-            var schoolScheduleVic = new SchoolSchedule()
+            var schoolScheduleVic = new SchoolSchedule
             {
                 SchDays = new[]
                 {
@@ -44,7 +84,23 @@ namespace AJF.HomeScreen.WebFront.Controllers
                         {
                             new SchItem {FromTime = "8.00", ToTime = "8.30", What = "M-BÅN"},
                             new SchItem {FromTime = "8.30", ToTime = "9.15", What = "IDR"},
-                            new SchItem {FromTime = "9.15", ToTime = "10.00", What = "IDR"},
+                            new SchItem {FromTime = "9.15", ToTime = "10.00", What = "IDR"}
+                        }
+                    },
+                    new SchDay
+                    {
+                        DayOfWeek = DayOfWeek.Tuesday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
+                        }
+                    },
+                    new SchDay
+                    {
+                        DayOfWeek = DayOfWeek.Wednesday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
                         }
                     },
                     new SchDay
@@ -60,39 +116,53 @@ namespace AJF.HomeScreen.WebFront.Controllers
                             new SchItem {FromTime = "11.50", ToTime = "12.20", What = "-Spis-"},
                             new SchItem {FromTime = "12.40", ToTime = "13.25", What = "BIL"},
                             new SchItem {FromTime = "13.25", ToTime = "14.10", What = "KRI"},
-                            new SchItem {FromTime = "14.10", ToTime = "14.55", What = "IDR/IDR"},
+                            new SchItem {FromTime = "14.10", ToTime = "14.55", What = "IDR/IDR"}
                         }
                     },
                     new SchDay
                     {
-                    DayOfWeek = DayOfWeek.Friday,
-                    SchItems = new[]
+                        DayOfWeek = DayOfWeek.Friday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.30", What = "M-BÅN"},
+                            new SchItem {FromTime = "8.30", ToTime = "9.15", What = "MAT/STUD"},
+                            new SchItem {FromTime = "9.15", ToTime = "10.00", What = "DAN"},
+                            new SchItem {FromTime = "10.20", ToTime = "11.05", What = "DAN"},
+                            new SchItem {FromTime = "11.05", ToTime = "11.50", What = "ENG"},
+                            new SchItem {FromTime = "11.50", ToTime = "12.20", What = "-Spis-"},
+                            new SchItem {FromTime = "12.40", ToTime = "13.25", What = "BIL"},
+                            new SchItem {FromTime = "13.25", ToTime = "14.10", What = "KRI"},
+                            new SchItem {FromTime = "14.10", ToTime = "14.55", What = "IDR/IDR"}
+                        }
+                    },
+                    new SchDay
                     {
-                        new SchItem {FromTime = "8.00", ToTime = "8.30", What = "M-BÅN"},
-                        new SchItem {FromTime = "8.30", ToTime = "9.15", What = "MAT/STUD"},
-                        new SchItem {FromTime = "9.15", ToTime = "10.00", What = "DAN"},
-                        new SchItem {FromTime = "10.20", ToTime = "11.05", What = "DAN"},
-                        new SchItem {FromTime = "11.05", ToTime = "11.50", What = "ENG"},
-                        new SchItem {FromTime = "11.50", ToTime = "12.20", What = "-Spis-"},
-                        new SchItem {FromTime = "12.40", ToTime = "13.25", What = "BIL"},
-                        new SchItem {FromTime = "13.25", ToTime = "14.10", What = "KRI"},
-                        new SchItem {FromTime = "14.10", ToTime = "14.55", What = "IDR/IDR"},
-                    }
+                        DayOfWeek = DayOfWeek.Saturday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
+                        }
+                    },
+                    new SchDay
+                    {
+                        DayOfWeek = DayOfWeek.Sunday,
+                        SchItems = new[]
+                        {
+                            new SchItem {FromTime = "8.00", ToTime = "8.55", What = "US"}
+                        }
                     }
                 }
-
             };
-            var isabellaDay = schoolScheduleIsa.SchDays.Single(x=>x.DayOfWeek==DateTime.Today.DayOfWeek);
+            var isabellaDay = schoolScheduleIsa.SchDays.Single(x => x.DayOfWeek == DateTime.Today.DayOfWeek);
             var victoriaDay = schoolScheduleVic.SchDays.Single(x => x.DayOfWeek == DateTime.Today.DayOfWeek);
 
             return View(new IndexModel
             {
                 SchOwners = new[]
                 {
-                    new SchOwner(){Name="Isabella", SchDay = isabellaDay},
-                    new SchOwner(){Name="Victoria", SchDay = victoriaDay}
+                    new SchOwner {Name = "Isabella", SchDay = isabellaDay},
+                    new SchOwner {Name = "Victoria", SchDay = victoriaDay}
                 }
-
             });
         }
     }

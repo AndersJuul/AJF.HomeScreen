1c2699cac4b3ae985f1edd4462cf3758a02b1ede
Anders Juul <andersjuulsfirma@gmail.com>
2020-05-26 19:53:40 +0200

    More tense screen -- added google keep, doesn't work

--- Src/AJF.HomeScreen.WebFront/Controllers/HomeController.cs
+++ Src/AJF.HomeScreen.WebFront/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using AJF.HomeScreen.WebFront.Models;
 
 namespace AJF.HomeScreen.WebFront.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
